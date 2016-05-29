f3be524b8ee33ce62622f014cccc52d0079b1d91
saanyrock <saanyrock@gmail.com>
2016-05-29 11:56:28 -0300

    skaposka

--- public_html/paginas/fotografia.php
+++ public_html/paginas/fotografia.php
@@ -40,28 +40,28 @@
                             <ul>
                                 <li><a href="#"><img src="images/thumbs/1.jpg" data-large="images/1.jpg" alt="image01" data-description="Fotografia com camera analógica tirada no Parque Flamboyant" /></a></li>
                                 <li><a href="#"><img src="images/thumbs/2.jpg" data-large="images/2.jpg" alt="image02" data-description="Fotografia com camera analógica tirada no Parque Flamboyant" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/3.jpg" data-large="images/3.jpg" alt="image03" data-description="Light Painting fotografado em aula no auditorio da Faculdade" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/3.jpg" data-large="images/3.jpg" alt="image03" data-description="Fotografia tirada no ensaio em estúdio" /></a></li>
                                 <li><a href="#"><img src="images/thumbs/4.jpg" data-large="images/4.jpg" alt="image04" data-description="Fotografia tirada no ensaio em estúdio" /></a></li>
                                 <li><a href="#"><img src="images/thumbs/5.jpg" data-large="images/5.jpg" alt="image05" data-description="Fotografia tirada no ensaio em estúdio" /></a></li>
                                 <li><a href="#"><img src="images/thumbs/6.jpg" data-large="images/6.jpg" alt="image06" data-description="Fotografia tirada no ensaio em estúdio" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/7.jpg" data-large="images/7.jpg" alt="image07" data-description="Fotografia tirada no ensaio em estúdio" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/8.jpg" data-large="images/8.jpg" alt="image08" data-description="Fotografia de Produto tirado em ensaio no estúdio" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/9.jpg" data-large="images/9.jpg" alt="image09" data-description= "Light Painting fotografado em aula no auditorio da Faculdade" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/10.jpg" data-large="images/10.jpg" alt="image10" data-description="Which fortified her visage from the sun" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/11.jpg" data-large="images/11.jpg" alt="image11" data-description="Whereon the thought might think sometime it saw" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/7.jpg" data-large="images/7.jpg" alt="image07" data-description="Aula em campo com fotografia de Modelo no Parque Multirama" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/8.jpg" data-large="images/8.jpg" alt="image08" data-description="Aula em campo com fotografia de Modelo no Parque Multirama" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/9.jpg" data-large="images/9.jpg" alt="image09" data-description= "Fotografia de Modelo no Parque Vaca Brava Pr�tica em Casa" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/10.jpg" data-large="images/10.jpg" alt="image10" data-description="Fotografia de Modelo no Parque Vaca Brava Pr�tica em Casa" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/11.jpg" data-large="images/11.jpg" alt="image11" data-description="Fotografia de Modelo no Parque Vaca Brava Pr�tica em Casa" /></a></li>
                                 <li><a href="#"><img src="images/thumbs/12.jpg" data-large="images/12.jpg" alt="image12" data-description="The carcass of beauty spent and done" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/13.jpg" data-large="images/13.jpg" alt="image13" data-description="Time had not scythed all that youth begun" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/14.jpg" data-large="images/14.jpg" alt="image14" data-description="Nor youth all quit; but, spite of heaven's fell rage" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/15.jpg" data-large="images/15.jpg" alt="image15" data-description="Some beauty peep'd through lattice of sear'd age" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/16.jpg" data-large="images/16.jpg" alt="image16" data-description="Oft did she heave her napkin to her eyne" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/17.jpg" data-large="images/17.jpg" alt="image17" data-description="Which on it had conceited characters" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/18.jpg" data-large="images/18.jpg" alt="image18" data-description="Laundering the silken figures in the brine" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/19.jpg" data-large="images/19.jpg" alt="image19" data-description="That season'd woe had pelleted in tears" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/20.jpg" data-large="images/20.jpg" alt="image20" data-description="And often reading what contents it bears" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/21.jpg" data-large="images/21.jpg" alt="image21" data-description="As often shrieking undistinguish'd woe" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/22.jpg" data-large="images/22.jpg" alt="image22" data-description="In clamours of all size, both high and low" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/23.jpg" data-large="images/23.jpg" alt="image23" data-description="Sometimes her levell'd eyes their carriage ride" /></a></li>
-                                <li><a href="#"><img src="images/thumbs/24.jpg" data-large="images/24.jpg" alt="image24" data-description="As they did battery to the spheres intend" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/13.jpg" data-large="images/13.jpg" alt="image13" data-description="Light Painting fotografado em aula no auditorio da Faculdade" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/14.jpg" data-large="images/14.jpg" alt="image14" data-description="Light Painting fotografado em aula no auditorio da Faculdade" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/15.jpg" data-large="images/15.jpg" alt="image15" data-description="Foto Tirada pela Cidade" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/16.jpg" data-large="images/16.jpg" alt="image16" data-description="Foto Tirada pela Cidade" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/17.jpg" data-large="images/17.jpg" alt="image17" data-description="Foto Tirada pela Cidade" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/18.jpg" data-large="images/18.jpg" alt="image18" data-description="Foto Tirada pela Cidade" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/19.jpg" data-large="images/19.jpg" alt="image19" data-description="Foto Tirada pela Cidade" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/20.jpg" data-large="images/20.jpg" alt="image20" data-description="Fotografia de Produto tirado em ensaio no estúdio" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/21.jpg" data-large="images/21.jpg" alt="image21" data-description="Fotografia de Produto tirado em pr�tica em casa" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/22.jpg" data-large="images/22.jpg" alt="image22" data-description="Fotografia de Produto tirado em ensaio no estúdio" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/23.jpg" data-large="images/23.jpg" alt="image23" data-description="Fotografia de Produto tirado em ensaio no estúdio" /></a></li>
+                                <li><a href="#"><img src="images/thumbs/24.jpg" data-large="images/24.jpg" alt="image24" data-description="Fotografia de Produto tirado em pr�tica em casa" /></a></li>
                             </ul>
                         </div>
                     </div>
