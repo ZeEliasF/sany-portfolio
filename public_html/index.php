c636e867219bb627972175018d40745aa9cecee9
ZeEliasF <zeeliasf@gmail.com>
2016-05-29 11:10:03 -0300

    Galeria funcionando

--- public_html/index.php
+++ public_html/index.php
@@ -12,9 +12,40 @@ require_once 'class/trocaURL.php';
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/estilo.css">
+        <link rel="stylesheet" type="text/css" href="css/elastislide.css">
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
         <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
         <script type="text/javascript" src="js/menu.js"></script>
         <script type="text/javascript" src="js/responsivo.js"></script>
+        <script type="text/javascript" src="js/jquery.tmpl.min.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/jquery.elastislide.js"></script>
+        <script type="text/javascript" src="js/gallery.js"></script><noscript>
+        <style>
+            .es-carousel ul{
+                display:block;
+            }
+        </style>
+        </noscript>
+        <script id="img-wrapper-tmpl" type="text/x-jquery-tmpl">	
+            <div class="rg-image-wrapper">
+            {{if itemsCount > 1}}
+            <div class="rg-image-nav">
+            <a href="#" class="rg-image-nav-prev">Previous Image</a>
+            <a href="#" class="rg-image-nav-next">Next Image</a>
+            </div>
+            {{/if}}
+            <div class="rg-image"></div>
+            <div class="rg-loading"></div>
+            <div class="rg-caption-wrapper">
+            <div class="rg-caption" style="display:none;">
+            <p></p>
+            </div>
+            </div>
+            </div>
+        </script>
     </head>
     <body onresize="responsivo()" onload="responsivo()">
 
